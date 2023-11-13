66faa10ccf75efc5ea1e032cca278ea57b40d139
Momay <panatchakorn@frappet.com>
2023-11-13 17:41:18 +0700

    up

--- hw_random.js
+++ hw_random.js
@@ -4,13 +4,15 @@ const { stdin: input, stdout: output } = require('process');
 async function main() {
     const rl = readline.createInterface({ input, output });
     const Random = Math.floor(Math.random() * 101);
+    console.log(Random);
     let numm = 0;
 
     while(true){
-        const answer = await rl.question('กรุณากรอกหมายเลข 1- 100:  ');
+        const answer = parseInt(await rl.question('กรุณากรอกหมายเลข 1- 100:  '));
         numm++;
+        console.log(answer);
 
-        if(answer == Random){
+        if(answer === Random){
             console.log("คุณทายถูก!");
             break;
           }else if(answer < Random){
